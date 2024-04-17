30a5e07158961206f3bfa776986ce8274931bc44
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2024-04-17 22:37:38 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("enter your number between 1 to 6");
+
+const randomNumber=Math.floor(Math.random()*6+1)
+const answer=await inquirer.prompt([
+    {
+        name:"userGuessedNumber",
+        type:"number",
+        message:"please guess a number between 1-6:"
+}
+]);
+if (answer.userGuessedNumber===randomNumber){
+    console.log("congratulation! you guessed right number");
+}
+else{
+     console.log("wrong guessed! try again");
+}
